8f6bf76bf5995c3751c91c3c4e64eeb04df79f4f
vkwalker31 <kirya31pro@gmail.com>
2026-02-24 02:57:27 +0300

    feat: initialize Kotlin Gradle orders project

---
+++ materials/orders-kotlin/settings.gradle.kts
@@ -0,0 +1,10 @@
+pluginManagement {
+    plugins {
+        kotlin("jvm") version "2.3.10"
+    }
+}
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+}
+rootProject.name = "orders-kotlin"
+include(/* ...projectPaths = */ ":app")
